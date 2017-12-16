d7efc13ef0c25b865a79ccd7ce9487b30ccd626f
Rafał Orłowski <rafal.orlowski@silvair.com>
2017-12-16 14:48:55 +0100

    (terraform)[]: additional subnets

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,29 +6,56 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "subnet_1" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.1.0/24"
   availability_zone = "eu-west-1a"
   tags {
-    Name = "subnet_1"
+    Name = "public_subnet_1"
   }
 }
 
-resource "aws_subnet" "subnet_2" {
+resource "aws_subnet" "public_subnet_2" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.2.0/24"
   availability_zone = "eu-west-1b"
   tags {
-    Name = "subnet_2"
+    Name = "public_subnet_2"
   }
 }
 
-resource "aws_subnet" "subnet_3" {
+resource "aws_subnet" "public_subnet_3" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.3.0/24"
   availability_zone = "eu-west-1c"
   tags {
-    Name = "subnet_3"
+    Name = "public_subnet_3"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "eu-west-1a"
+  tags {
+    Name = "private_subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "eu-west-1b"
+  tags {
+    Name = "private_subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "eu-west-1c"
+  tags {
+    Name = "private_subnet_3"
   }
 }
