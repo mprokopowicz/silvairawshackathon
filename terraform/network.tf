2e6356f8ced98e652c40568152b957ed3b87b4fa
Rafał Orłowski <rafal.orlowski@silvair.com>
2017-12-16 13:08:25 +0100

    (terraform)[]: go west ;)

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "subnet_1" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-central-1a"
+  availability_zone = "eu-west-1a"
   tags {
     Name = "subnet_1"
   }
@@ -18,7 +18,7 @@ resource "aws_subnet" "subnet_1" {
 resource "aws_subnet" "subnet_2" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.2.0/24"
-  availability_zone = "eu-central-1b"
+  availability_zone = "eu-west-1b"
   tags {
     Name = "subnet_2"
   }
@@ -27,7 +27,7 @@ resource "aws_subnet" "subnet_2" {
 resource "aws_subnet" "subnet_3" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "10.0.3.0/24"
-  availability_zone = "eu-central-1c"
+  availability_zone = "eu-west-1c"
   tags {
     Name = "subnet_3"
   }
